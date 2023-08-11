75e98ae5f8253639c5aee74a74090f4ff600bd9a
Wellington Ideão <wellington.ideao@gmail.com>
2023-08-10 22:59:31 -0300

    Implementado pool de conexão

--- src/main/java/com/ideao/ConnectionFactory.java
+++ src/main/java/com/ideao/ConnectionFactory.java
@@ -4,10 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+    private DataSource dataSource;
+
+    public ConnectionFactory() {
+        // pool de
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("dbsql123");
+        this.dataSource = comboPooledDataSource;
+    }
     
     public Connection getConnection() throws SQLException{
-        return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?" +
-            "useTimezone=true&serverTimezone", "root", "dbsql123");
+       return this.dataSource.getConnection();
     }
 }
